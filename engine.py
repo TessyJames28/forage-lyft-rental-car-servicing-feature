83b0b4063c1794a8877f434c1649e7b36ab0a5d7
TessyJames28 <tessyjames28@gmail.com>
2024-02-23 13:12:14 +0100

    improved the engine file

--- engine.py
+++ engine.py
@@ -5,28 +5,28 @@ class Engine:
 
 # Capulet Engine needs servicing once every 30,000 miles
 class CapuletEngine(Engine):
-    def __init__(self, last_service_milleage, current_milleage):
+    def __init__(self, last_service_mileage, current_mileage):
         super().__init__()
-        self.last_service_milleage = last_service_milleage
-        self.current_milleage = current_milleage
+        self.last_service_mileage = last_service_mileage
+        self.current_mileage = current_mileage
         
         
     def needs_servicing(self):
-        total_milleage = self.last_service_milleage + self.current_milleage
-        return total_milleage >= 30000
+        milleage = self.current_mileage - self.last_service_mileage
+        return milleage >= 30000
 
 
 # Willoughby Engine needs servicing once every 60,000 miles
 class WilloughbyEngine(Engine):
-    def __init__(self, last_service_milleage, current_milleage):
+    def __init__(self, last_service_mileage, current_mileage):
         super().__init__()
-        self.last_service_milleage = last_service_milleage
-        self.current_milleage = current_milleage
+        self.last_service_mileage = last_service_mileage
+        self.current_mileage = current_mileage
         
         
     def needs_servicing(self):
-        total_milleage = self.last_service_milleage + self.current_milleage
-        return total_milleage >= 60000
+        milleage = self.current_mileage - self.last_service_mileage
+        return milleage >= 60000
 
 
 
