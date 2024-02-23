8688c4f6d95f865cd8293e0e6b57a23fea1daf3d
TessyJames28 <tessyjames28@gmail.com>
2024-02-23 13:52:03 +0100

    module import

--- car_factory.py
+++ car_factory.py
@@ -1,5 +1,10 @@
 # Cars are created by calling the corresponding CarFactory
 from car import Car
+from battery.nubbin_battery import NubbinBattery
+from battery.spindler_battery import SpindlerBattery
+from engine.capulet_engine import CapuletEngine
+from engine.willoughby_engine import WilloughbyEngine
+from engine.sternman_engine import SternmanEngine
 
 
 class CarFactory:
@@ -8,8 +13,8 @@ class CarFactory:
     @staticmethod
     def create_calliope(self, current_date, last_service_date, current_mileage, last_service_mileage):
         # create instances for Capulet engine and Spindler battery
-        engine = self.engine.CapuletEngine(last_service_mileage, current_mileage)
-        battery = self.battery.SpindlerBattery(last_service_date, current_date)
+        engine = CapuletEngine(last_service_mileage, current_mileage)
+        battery = SpindlerBattery(last_service_date, current_date)
         
         # creates a car instances using engine and battery
         calliope = Car(engine, battery)
@@ -20,8 +25,8 @@ class CarFactory:
     @staticmethod
     def create_glissade(self, current_date, last_service_date, current_mileage, last_service_mileage):
         # create instances for Willoughby engine and Spindler battery
-        engine = self.engine.WilloughbyEngine(last_service_mileage, current_mileage)
-        battery = self.battery.SpindlerBattery(last_service_date, current_date)
+        engine = WilloughbyEngine(last_service_mileage, current_mileage)
+        battery = SpindlerBattery(last_service_date, current_date)
         
         # creates a car instances using engine and battery
         glissade = Car(engine, battery)
@@ -33,8 +38,8 @@ class CarFactory:
     @staticmethod
     def create_palindrome(self, current_date, last_service_date, warning_light_on):
         # create instances for Sternman engine and Spindler battery
-        engine = self.engine.SternmanEngine(warning_light_on)
-        battery = self.battery.SpindlerBattery(last_service_date, current_date)
+        engine = SternmanEngine(warning_light_on)
+        battery = SpindlerBattery(last_service_date, current_date)
         
         # creates a car instances using engine and battery
         palindrome = Car(engine, battery)
@@ -46,8 +51,8 @@ class CarFactory:
     @staticmethod
     def create_rorschach(self, current_date, last_service_date, current_mileage, last_service_mileage):
         # create instances for Willoughby engine and Nubbin battery
-        engine = self.engine.WilloughbyEngine(last_service_mileage, current_mileage)
-        battery = self.battery.NubbinBattery(last_service_date, current_date)
+        engine = WilloughbyEngine(last_service_mileage, current_mileage)
+        battery = NubbinBattery(last_service_date, current_date)
         
         # creates a car instances using engine and battery
         rorschach = Car(engine, battery)
@@ -59,8 +64,8 @@ class CarFactory:
     @staticmethod
     def create_thovex(self, current_date, last_service_date, current_mileage, last_service_mileage):
         # create instances for Capulet engine and Nubbin battery
-        engine = self.engine.CapuletEngine(last_service_mileage, current_mileage)
-        battery = self.battery.NubbinBattery(last_service_date, current_date)
+        engine = CapuletEngine(last_service_mileage, current_mileage)
+        battery = NubbinBattery(last_service_date, current_date)
         
         # creates a car instances using engine and battery
         thovex = Car(engine, battery)
